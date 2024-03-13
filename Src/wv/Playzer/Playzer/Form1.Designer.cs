9530545437c944d2dcb82ed3e0957e9899c661a1
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-03-13 11:57:48 +0100

    Fix bugs

--- Src/wv/Playzer/Playzer/Form1.Designer.cs
+++ Src/wv/Playzer/Playzer/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 60;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // notifyIcon1
